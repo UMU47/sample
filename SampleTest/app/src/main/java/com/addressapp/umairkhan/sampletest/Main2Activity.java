f917d446639bfb93d602646884154b34560adf49
UMU47 <khanumair191997@gmail.com>
2018-07-28 15:33:47 +0530

    Made Changes

--- SampleTest/app/src/main/java/com/addressapp/umairkhan/sampletest/Main2Activity.java
+++ SampleTest/app/src/main/java/com/addressapp/umairkhan/sampletest/Main2Activity.java
@@ -1,27 +1,22 @@
 package com.addressapp.umairkhan.sampletest;
 
+import android.annotation.SuppressLint;
 import android.graphics.Rect;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
-
-    private int lasttop=0;
-
-    public void parallax(final View v){
-        final Rect r=new Rect();
-        v.getLocalVisibleRect(r);
-        if(lasttop!=r.top){
-            lasttop=r.top;
-            v.post(new Runnable() {
-                @Override
-                public void run() {
-                    v.setY((float)(r.top/2.0));
-                }
-            });
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
